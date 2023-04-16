ab81613c8a3d8d928d9080ed25647e840991ad7d
KelvenB <104724730+KelvenB@users.noreply.github.com>
2023-04-16 13:27:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var moveButton = document.querySelector(".nao");
+
+var correctlyButton = document.querySelector(".sim");
+
+moveButton.addEventListener("click", function () {
+    var vertical = Math.floor(Math.random() * 300);
+    var horizontal = Math.floor(Math.random() * 300);
+    moveButton.style.position = "absolute";
+    moveButton.style.left = vertical + "px";
+    moveButton.style.top = horizontal + "px";
+});
+
+correctlyButton.addEventListener("click", function() {
+    alert("Eu pago o lanche e vc o boquet");
+});
+
+
